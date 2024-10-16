fd0b09247e04f54078915d50f9411bee9400e542
valeriabukova <valeriasergeevna688@gmail.com>
2024-10-16 22:52:02 +0300

    Initial Commit

---
+++ homework valeria class new matrixxx/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework valeria class new matrixxx
+//
+//  Created by Valeria  Bukova on 16.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
